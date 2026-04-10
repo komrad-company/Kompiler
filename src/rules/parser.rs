68c5d59430a506f3c24f6489fc733e0b96fef756
wampixel <wampixel@gmail.com>
2026-04-10 22:30:49 +0200

    [FIX] Clippy error

--- src/rules/parser.rs
+++ src/rules/parser.rs
@@ -7,7 +7,7 @@ pub fn parse_rules(path: &Path) -> Result<Vec<Rule>, Error> {
     let rules = fs::read_dir(path)?
         .flatten()
         .filter_map(|e| {
-            fs::File::open(&e.path())
+            fs::File::open(e.path())
                 .map_err(|open| eprintln!("Opening {}: {}", e.path().display(), open))
                 .ok()
                 .and_then(|file| {
