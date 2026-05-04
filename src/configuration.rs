b9500c89e7d0fc720e8b99d74589d35c42596420
wampixel <wampixel@gmail.com>
2026-05-04 22:14:37 +0200

    [DEL] configuration moved on korelator

--- src/configuration.rs
+++
@@ -1,9 +0,0 @@
-use serde::Deserialize;
-
-use khronika::configuration::TelemetryConfiguration;
-
-#[derive(Deserialize)]
-pub struct Configuration {
-    pub rules_path: String,
-    pub log: TelemetryConfiguration,
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
-use std::{env, error::Error, fs::File, io::BufReader};
+use std::error::Error;
 
 use serde::Deserialize;
-use serde_json::from_reader;
 
-use crate::errors::UnforgivableErrors;
-
-pub mod configuration;
 pub mod errors;
 pub mod rules;
 
@@ -20,20 +16,4 @@ pub enum RuleLevel {
     Medium,
     High,
     Critical,
-}
-
-pub fn load_configuration() -> Result<configuration::Configuration, UnforgivableErrors> {
-    let configuration_path: String =
-        env::var("CONFIGURATION_PATH").unwrap_or_else(|_| "configuration.json".to_string());
-
-    let file = File::open(&configuration_path).map_err(|_| {
-        UnforgivableErrors::MissingConfigurationFile {
-            path: configuration_path,
-        }
-    })?;
-
-    let reader = BufReader::new(file);
-    let conf = from_reader(reader).map_err(UnforgivableErrors::InvalidFormat)?;
-
-    Ok(conf)
 }
